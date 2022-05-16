13a7bd0e16250deb90c852eee76415240a0a90a1
OxMarco <>
2022-05-16 22:00:38 +0200

    solved stack too deep

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -19,6 +19,7 @@ contract Strategy {
     error Input_Balance_Error();
     error Output_Balance_Error();
     error Allowance_Error();
+    error Vault_Not_Found(address token);
 
     IAaveLendingPool private immutable aavePool;
     IAaveProtocolDataProvider private immutable aaveData;
@@ -41,50 +42,72 @@ contract Strategy {
         apwineAMMRegistry = IApwineAMMRegistry(_apwineAMMRegistry);
     }
 
-    function invest(address tkn, uint256 amount) external returns(uint256) {
-        IERC20 token = IERC20(tkn);
+    function _transferTokens(IERC20 token, uint256 amount) internal {
         if (token.balanceOf(msg.sender) < amount) revert Input_Balance_Error();
         if (token.allowance(msg.sender, address(this)) < amount) revert Allowance_Error();
 
-        // Step 1 - take tokens from the user
         token.safeTransferFrom(msg.sender, address(this), amount);
-        if(token.allowance(address(this), address(aavePool)) == 0)
-            token.safeApprove(address(aavePool), type(uint256).max); // approve aave
+    }
 
-        // Step 2 - deposit wanted token on Aave
+    function _depositOnAave(IERC20 token, uint256 amount) internal returns(IERC20) {
+        if(token.allowance(address(this), address(aavePool)) == 0)
+            token.safeApprove(address(aavePool), type(uint256).max);
+    
         aavePool.deposit(address(token), amount, address(this), 0);
 
-        // Step 3 - get Apwine data
-        (address futureVault, uint256 pairID) = _getFutureVault(tkn);
-        IApwineAMM amm = IApwineAMM(apwineAMMRegistry.getFutureAMMPool(futureVault));
+        (address tkn,,)= aaveData.getReserveTokensAddresses(address(token));
+        return IERC20(tkn);
+    }
 
-        // Step 4 - deposit aTokens on Apwine
-        (address aToken,,) = aaveData.getReserveTokensAddresses(tkn);
-        if(IERC20(aToken).allowance(address(this), address(apwineController)) == 0)
-            IERC20(aToken).safeApprove(address(apwineController), type(uint256).max); // approve apwine controller
+    function _depositOnApwine(address futureVault, IERC20 aToken, uint256 amount) internal {
+        if(aToken.allowance(address(this), address(apwineController)) == 0)
+            aToken.safeApprove(address(apwineController), type(uint256).max); // approve apwine controller
 
         apwineController.deposit(futureVault, amount);
+    }
 
-        // Step 4 - swap PTokens for the underlying wanted tokens
+    function _swapOnApwine(address futureVault, uint256 pairID) internal returns (uint256) {
         IERC20 ptoken = IERC20(IApwineFutureVault(futureVault).getPTAddress());
         uint256 ptokenBalance = ptoken.balanceOf(address(this));
+
+        IApwineAMM amm = IApwineAMM(apwineAMMRegistry.getFutureAMMPool(futureVault));
         (uint256 amountOut, ) = amm.swapExactAmountIn(
             pairID,
             0, // _tokenIn - this should be the ptoken
             ptokenBalance,
             1, // _tokenOut -  this should be the underlying
-            amm.getSpotPrice(pairID, 0, 1) * ptokenBalance, // TBD check math, 0 (ptoken) and 1 (fytoken) should be repaced with tokenIDs
+            amm.getSpotPrice(pairID, 0, 1) * ptokenBalance,
             address(this)
         );
+
+        return amountOut;
+    }
+
+    function invest(address tkn, uint256 amount, address futureVault, uint256 pairID) external returns(uint256) {
+        IERC20 token = IERC20(tkn);
+    
+        // Step 1 - take tokens from the user
+        _transferTokens(token, amount);
+
+        // Step 2 - deposit wanted token on Aave
+        IERC20 aToken = _depositOnAave(token, amount);
+        
+        // Step 3 - deposit aTokens on Apwine
+        _depositOnApwine(futureVault, aToken, amount);
+        
+        // Step 4 - swap PTokens for the underlying wanted tokens
+        uint256 amountOut = _swapOnApwine(futureVault, pairID);
 
         if(token.balanceOf(address(this)) != amountOut) revert Output_Balance_Error();
 
         return amountOut;
     }
 
-    function _getFutureVault(address token) internal view returns (address, uint256) {
+    function getFutureVault(address token) external view returns (address, uint256) {
         for(uint256 i = 0; i < apwineRegistry.futureVaultCount(); i++) {
             if(IApwineFutureVault(apwineRegistry.getFutureVaultAt(i)).getIBTAddress() == token) return (apwineRegistry.getFutureVaultAt(i), i);
         }
+
+        revert Vault_Not_Found(token);
     }
 }
--- test/strategy/Strategy.behavior.ts
+++ test/strategy/Strategy.behavior.ts
@@ -7,6 +7,8 @@ export function shouldInvest(): void {
     const amount = ethers.utils.parseUnits("1000.0", 6); // USDC has got 6 decimal places
     getTokens(this.signers.investor.address, USDT, USDTWhale, amount.mul(100));
 
-    expect( await this.strategy.connect(this.signers.investor).invest(USDT, amount) ).to.equal(amount);
+    const res = await this.strategy.getFutureVault(USDT);
+    console.log(res);
+    expect( await this.strategy.connect(this.signers.investor).invest(USDT, amount, res[0], res[1]) ).to.equal(amount);
   });
 }
